d3e5ace9f783c7c4c370c781685694b157951ecb
SlaatsM <madelief.slaats@ru.nl>
2025-01-11 12:59:25 +0100

    Assignment 4: Part 2

--- src/basiclearner/ExampleExperiment.java
+++ src/basiclearner/ExampleExperiment.java
@@ -5,6 +5,7 @@ import de.learnlib.api.SUL;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.net.InetAddress;
 
 /**
  * Created by ramon on 13-12-16.
@@ -20,15 +21,26 @@ public class ExampleExperiment {
         // For a SUL over a socket, use the SocketSUL-class
         // You can also program an own SUL-class if you extend SUL<String,String> (or SUL<S,T> in
         // general, with S and T the input and output types - but this class assumes strings)
-        System.out.println("hello");
-        SUL<String,String> sul = new ExampleSUL();
+    	// Replace ExampleSUL with SocketSUL
+        InetAddress ipAddress = InetAddress.getLoopbackAddress();
+        int port = 7892; 
+        String resetCommand = "reset";
 
-        // the input alphabet
-        Collection<String> inputAlphabet = ImmutableSet.of("a", "b", "c");
+        SUL<String, String> sul = new SocketSUL(ipAddress, port, true, resetCommand);
+
+        // Define the input alphabet
+        Collection<String> inputAlphabet = ImmutableSet.of(
+        		"IACK", "IREQ_0_0_0", "IREQ_0_0_1", "IREQ_0_1_0",
+        		"IREQ_0_1_1", "IREQ_1_0_0", "IREQ_1_0_1", "IREQ_1_1_0", "IREQ_1_1_1",
+        		"ISENDFRAME", "ITIMEOUT"
+                );
 
         try {
+        	BasicLearner.randomWalk_chanceOfResetting = 0; 
+        	BasicLearner.randomWalk_numberOfSymbols = 75;   
+
             // runControlledExperiment for detailed statistics, runSimpleExperiment for just the result
-            BasicLearner.runControlledExperiment(sul, BasicLearner.LearningMethod.LStar, BasicLearner.TestingMethod.WpMethod, inputAlphabet);
+            BasicLearner.runControlledExperiment(sul, BasicLearner.LearningMethod.RivestSchapire, BasicLearner.TestingMethod.RandomWalk, inputAlphabet);
         } finally {
             if (sul instanceof AutoCloseable) {
                 try {
